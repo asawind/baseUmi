546d09d9784c0bbec1f08aa858c389e08c7081a8
zhangqi <554309541@qq.com>
2021-10-04 21:17:51 +0800

    update content

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,11 +1,71 @@
-import styles from './index.css';
+import styles from './index.less';
+import { Layout, Menu, Button } from 'antd';
+import {
+  AppstoreOutlined,
+  BarChartOutlined,
+  CloudOutlined,
+  ShopOutlined,
+  TeamOutlined,
+  UserOutlined,
+  UploadOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+
+const { Header, Content, Footer, Sider } = Layout;
 
 function BasicLayout(props) {
   return (
-    <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to umi!</h1>
-      {props.children}
-    </div>
+    // <div className={styles.normal}>
+    //   <h1 className={styles.title}>Yay! Welcome to umi!</h1>
+    //   {props.children}
+    // </div>
+    <Layout>
+      <Sider
+        style={{
+          overflow: 'auto',
+          height: '100vh',
+          position: 'fixed',
+          left: 0,
+        }}
+      >
+        <div className="logo" />
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+          <Menu.Item key="1" icon={<UserOutlined />}>
+            nav 1
+        </Menu.Item>
+          <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+            nav 2
+        </Menu.Item>
+          <Menu.Item key="3" icon={<UploadOutlined />}>
+            nav 3
+        </Menu.Item>
+          <Menu.Item key="4" icon={<BarChartOutlined />}>
+            nav 4
+        </Menu.Item>
+          <Menu.Item key="5" icon={<CloudOutlined />}>
+            nav 5
+        </Menu.Item>
+          <Menu.Item key="6" icon={<AppstoreOutlined />}>
+            nav 6
+        </Menu.Item>
+          <Menu.Item key="7" icon={<TeamOutlined />}>
+            nav 7
+        </Menu.Item>
+          <Menu.Item key="8" icon={<ShopOutlined />}>
+            nav 8
+        </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout className="site-layout" style={{ marginLeft: 200 }}>
+        <Header className="site-layout-background" style={{ padding: 0 }} />
+        <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+          <div className="site-layout-background" style={{ padding: 24, textAlign: 'center' }}>
+{props.children}
+        </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+      </Layout>
+    </Layout>
   );
 }
 
